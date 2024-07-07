04feb6223e4fc0f0f86bd70b46168397f10f775d
Chihaya-Yuka <yuka-chihaya@outlook.com>
2024-07-07 14:33:04 +0800

    style: Refactoring using object-oriented methods

--- src/main.py
+++ src/main.py
@@ -4,67 +4,81 @@ import socket
 import threading
 import mimetypes
 
-# Server configuration
-HOST = '0.0.0.0'
-PORT = 8080
-DOCUMENT_ROOT = './public'  # Change this to your directory containing static files
+class HTTPRequestHandler:
+    def __init__(self, client_socket, document_root):
+        self.client_socket = client_socket
+        self.document_root = document_root
 
-# Helper function to send HTTP response
-def send_response(client_socket, status_code, content, content_type='text/html', compress=False):
-    response_line = f'HTTP/1.1 {status_code}\r\n'
-    headers = f'Content-Type: {content_type}\r\n'
-    headers += 'Connection: close\r\n'
-    if compress:
-        headers += 'Content-Encoding: deflate\r\n'
-        content = zlib.compress(content)
-    headers += f'Content-Length: {len(content)}\r\n'
-    headers += '\r\n'
-    response = response_line.encode() + headers.encode() + content
-    client_socket.sendall(response)
+    def handle_request(self):
+        try:
+            request = self.client_socket.recv(1024).decode('utf-8')
+            if not request:
+                return
 
-# Function to handle client requests
-def handle_client(client_socket):
-    try:
-        request = client_socket.recv(1024).decode('utf-8')
-        if not request:
-            return
+            request_line = request.splitlines()[0]
+            method, path, _ = request_line.split()
 
-        request_line = request.splitlines()[0]
-        method, path, _ = request_line.split()
+            if method != 'GET':
+                self.send_response('405 Method Not Allowed', b'Method Not Allowed')
+                return
 
-        if method != 'GET':
-            send_response(client_socket, '405 Method Not Allowed', b'Method Not Allowed')
-            return
+            if path == '/':
+                path = '/index.html'
 
-        if path == '/':
-            path = '/index.html'
+            file_path = os.path.join(self.document_root, path.lstrip('/'))
+            if not os.path.isfile(file_path):
+                self.send_response('404 Not Found', b'File Not Found')
+                return
 
-        file_path = os.path.join(DOCUMENT_ROOT, path.lstrip('/'))
-        if not os.path.isfile(file_path):
-            send_response(client_socket, '404 Not Found', b'File Not Found')
-            return
+            with open(file_path, 'rb') as file:
+                content = file.read()
 
-        with open(file_path, 'rb') as file:
-            content = file.read()
+            content_type, _ = mimetypes.guess_type(file_path)
+            self.send_response('200 OK', content, content_type or 'application/octet-stream', compress=True)
+        except Exception as e:
+            print(f'Error handling request: {e}')
+        finally:
+            self.client_socket.close()
 
-        content_type, _ = mimetypes.guess_type(file_path)
-        send_response(client_socket, '200 OK', content, content_type or 'application/octet-stream', compress=True)
-    except Exception as e:
-        print(f'Error handling request: {e}')
-    finally:
-        client_socket.close()
+    def send_response(self, status_code, content, content_type='text/html', compress=False):
+        response_line = f'HTTP/1.1 {status_code}\r\n'
+        headers = f'Content-Type: {content_type}\r\n'
+        headers += 'Connection: close\r\n'
+        if compress:
+            headers += 'Content-Encoding: deflate\r\n'
+            content = zlib.compress(content)
+        headers += f'Content-Length: {len(content)}\r\n'
+        headers += '\r\n'
+        response = response_line.encode() + headers.encode() + content
+        self.client_socket.sendall(response)
 
-# Function to start the server
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(5)
-    print(f'Serving HTTP on {HOST} port {PORT} (http://{HOST}:{PORT}/) ...')
+class HTTPServer:
+    def __init__(self, host, port, document_root):
+        self.host = host
+        self.port = port
+        self.document_root = document_root
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        threading.Thread(target=handle_client, args=(client_socket,)).start()
+    def start(self):
+        try:
+            self.server_socket.bind((self.host, self.port))
+            self.server_socket.listen(5)
+            print(f'Serving HTTP on {self.host} port {self.port} (http://{self.host}:{self.port}/) ...')
+
+            while True:
+                client_socket, client_address = self.server_socket.accept()
+                handler = HTTPRequestHandler(client_socket, self.document_root)
+                threading.Thread(target=handler.handle_request).start()
+        except Exception as e:
+            print(f'Error starting server: {e}')
+        finally:
+            self.server_socket.close()
 
 if __name__ == '__main__':
-    start_server()
+    HOST = '0.0.0.0'
+    PORT = 8080
+    DOCUMENT_ROOT = './public'
+
+    server = HTTPServer(HOST, PORT, DOCUMENT_ROOT)
+    server.start()
