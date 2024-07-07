2c07c181b9b97970238d5256dcef80eb234b34fe
Chihaya-Yuka <yuka-chihaya@outlook.com>
2024-07-07 17:06:15 +0800

    debug: Import _thread

--- src/init.py
+++ src/init.py
@@ -1,8 +1,10 @@
+import _thread
 import subprocess
 
 def start(filename:str) -> int:
     return subprocess.call(['python', filename])
 
-start('proxy.py')
-start('main.py')
 print('Copilot is running on http://127.0.0.1:2334.')
+_thread.start_new_thread(start,('main.py',))
+_thread.start_new_thread(start,('proxy.py',))
+while True: pass
