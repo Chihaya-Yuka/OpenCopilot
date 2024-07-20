57492e102c53da01f33d7ea0716c062edb2e0d6b
Chihaya-Yuka <yuka-chihaya@outlook.com>
2024-07-20 20:18:34 +0800

    feat: what thing i did???

--- opencopilot/node_modules/@vscode/test-electron/out/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { download, downloadAndUnzipVSCode, DownloadOptions } from './download';
-export { runTests, TestOptions, TestRunFailedError } from './runTest';
-export { resolveCliPathFromVSCodeExecutablePath, resolveCliArgsFromVSCodeExecutablePath, runVSCodeCommand, VSCodeCommandError, RunVSCodeCommandOptions, } from './util';
-export * from './progress.js';
